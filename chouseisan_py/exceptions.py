f8a9d900fde81128662f255cd41f46a79966eef1
Ryuji TSUTSUI <ryu22e@gmail.com>
2022-01-27 21:54:58 +0900

    Add documentation on source code

--- chouseisan_py/exceptions.py
+++ chouseisan_py/exceptions.py
@@ -1,15 +1,23 @@
+"""Errors that occurs during
+ `調整さん <https://chouseisan.com/>`_ (Chouseisan) operation."""
 from __future__ import annotations
 
 
 class LoginError(Exception):
-    # TODO Write docstring
+    """Error that occurs when login fails."""
+
     pass
 
 
 class TagNotFoundError(Exception):
-    # TODO Write docstring
+    """Error that occurs when the expected HTML tag
+    does not exist on `調整さん <https://chouseisan.com/>`_ (Chouseisan)."""
 
     def __init__(self, selector: str):
+        """Initialize the Chouseisan instance.
+
+        :param selector: The specified selector
+        """
         self._selector = selector
 
     def __str__(self):
---
+++ docs/chouseisan_py.rst
@@ -0,0 +1,29 @@
+chouseisan\_py package
+======================
+
+Submodules
+----------
+
+chouseisan\_py.chouseisan module
+--------------------------------
+
+.. automodule:: chouseisan_py.chouseisan
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+chouseisan\_py.exceptions module
+--------------------------------
+
+.. automodule:: chouseisan_py.exceptions
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: chouseisan_py
+   :members:
+   :undoc-members:
+   :show-inheritance:
