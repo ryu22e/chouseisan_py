c62ce2ea172d4c657694d20eb4dbf60edf83ba8e
Ryuji TSUTSUI <ryu22e@gmail.com>
2022-02-02 21:52:05 +0900

    Add configuration for Sphinx in .bumpversion.cfg

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, Ryuji Tsutsui"
 author = "Ryuji Tsutsui"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1"
+release = "0.1.0"
 
 
 # -- General configuration ---------------------------------------------------
