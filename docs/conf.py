22d141ad2663da844bba296689a9c591b3058ea4
Ryuji TSUTSUI <ryu22e@gmail.com>
2022-05-25 22:03:59 +0900

    Bump version: 0.1.0 → 0.1.1

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2021-2022, Ryuji Tsutsui"
 author = "Ryuji Tsutsui"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.0"
+release = "0.1.1"
 
 
 # -- General configuration ---------------------------------------------------
